1c87a63dcae5a7cfc8c2e8c52ea866d1d69d0ddf
hoki4 <128627987+hoki4@users.noreply.github.com>
2025-12-30 17:27:33 +0300

    Реализация переключения темы

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,5 +1,26 @@
-import {isWebP} from './modules/functions.js';
-import {test} from './modules/test.js';
+const btn = document.querySelector('.theme-toggle');
 
-isWebP();
-test();
+btn.addEventListener('click', () => {
+  const isDark = document.body.classList.contains('theme-dark');
+
+  if (isDark) {
+    document.body.classList.remove('theme-dark');
+    btn.classList.remove('is-dark');
+    btn.classList.add('is-light');
+    localStorage.setItem('musify_theme', 'light');
+  } else {
+    document.body.classList.add('theme-dark');
+    btn.classList.remove('is-light');
+    btn.classList.add('is-dark');
+    localStorage.setItem('musify_theme', 'dark');
+  }
+});
+
+const savedTheme = localStorage.getItem('musify_theme');
+
+if (savedTheme === 'dark') {
+  document.body.classList.add('theme-dark');
+  btn.classList.add('is-dark');
+} else {
+  btn.classList.add('is-light');
+}
